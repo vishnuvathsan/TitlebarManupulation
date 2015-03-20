3ece6a3a3019ac7077b6b1e5e5c9772ea4b737fb
Vishnuvathsasarma <vishnuvathsan@gmail.com>
2015-03-20 11:41:12 +0530

    view changes

--- app/src/main/java/com/vishnu/titlebarmanupulation/MainActivity.java
+++ app/src/main/java/com/vishnu/titlebarmanupulation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vishnu.titlebarmanupulation;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +13,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private ScrollView sv;
     private RelativeLayout rl;
@@ -27,14 +28,14 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         // Find the view
-        this.sv = (ScrollView) findViewById(R.id.scrollView);
-        sv.setSmoothScrollingEnabled(true);
+//        this.sv = (ScrollView) findViewById(R.id.scrollView);
+//        sv.setSmoothScrollingEnabled(true);
 //        sv.setNestedScrollingEnabled(true);
         this.rl = (RelativeLayout) findViewById(R.id.rl);
         this.tv1 = (TextView) findViewById(R.id.textView1);
         this.tv2 = (TextView) findViewById(R.id.textView2);
         this.listView = (ListView) findViewById(R.id.listView);
-        listView.setSmoothScrollbarEnabled(true);
+//        listView.setSmoothScrollbarEnabled(true);
 //        listView.setNestedScrollingEnabled(true);
 
         // Create the arrays
@@ -47,8 +48,6 @@ public class MainActivity extends ActionBarActivity {
         // Create an array adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, designPatterns);
         listView.setAdapter(adapter);
-
-
 
     }
 
